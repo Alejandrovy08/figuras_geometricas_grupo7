6d9bcd9d81158516614224b695782047d63b13e7
M. Angel Moreno <miguelangelmorenodelgadot@gmail.com>
2024-10-18 09:42:02 +0200

    constructor punto

--- Punto.php
+++ Punto.php
@@ -1,3 +1,12 @@
 <?php
+    class Punto{
+        private float $x;
+        private float $y;
 
+        public function __construct($x,$y){
+            $this->x = $x;
+            $this->y = $y; 
+        }
+
+    }
 ?>
